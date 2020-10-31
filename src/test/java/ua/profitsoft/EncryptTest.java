a344fbd021a026b793d233ba680f5dfd3131f9e3
vikaxx <victoria.aliaeva@gmail.com>
2020-10-31 15:11:08 +0200

    unit test

---
+++ src/test/java/ua/profitsoft/EncryptTest.java
@@ -0,0 +1,19 @@
+package ua.profitsoft;
+
+import junit.framework.TestCase;
+
+public class EncryptTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testMain() {
+    }
+
+    public void testEncrypt() {
+    }
+}
